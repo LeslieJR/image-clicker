e9c52f42bf29520a0ab6e65b3983fc0134c7eda6
LeslieJR <l.reyes.ubiqum@gmail.com>
2023-10-19 21:10:29 +0200

    Details component created
    method to show the selected superheroe details implemented

---
+++ src/components/SuperheroDetails.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps(['itemProps'])
+</script>
+<template>
+  <v-container>
+    <v-card class="mx-auto">
+      <v-img
+        :src="`${props.itemProps.thumbnail.path}.${props.itemProps.thumbnail.extension}`"
+        cover
+      />
+      <v-card-title class="wrap-title">
+        {{ props.itemProps.name }}
+      </v-card-title>
+    </v-card>
+  </v-container>
+</template>
+
+<style scoped>
+.wrap-title {
+  flex: auto;
+  white-space: normal;
+  word-break: break-word;
+}
+</style>
--- src/components/SuperheroesList.vue
+++ src/components/SuperheroesList.vue
@@ -1,5 +1,15 @@
 <script setup>
+import { ref } from 'vue'
+import SuperheroeDetails from './SuperheroDetails.vue'
 const props = defineProps(['items'])
+
+let selectedItem = ref(null)
+function showDetails(item) {
+  selectedItem.value = {
+    ...item,
+    clicks: item.clicks + 1
+  }
+}
 </script>
 
 <template>
@@ -12,12 +22,21 @@ const props = defineProps(['items'])
               class="d-flex align-center mx-auto"
               dark
               height="100px"
+              @click="showDetails(item)"
             >
               <v-card-title class="wrap-title">
                 {{ item.name }}
               </v-card-title>
             </v-card>
           </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3">
+        <v-row align="center" justify="center" v-if="selectedItem">
+          <SuperheroeDetails :itemProps="selectedItem" />
+        </v-row>
+        <v-row align="center" justify="center" v-else>
+          <p>Select an item to see details</p>
         </v-row>
       </v-col>
     </v-row>
